74c34431cb593b4daac82e6c50fd3dc0fa5d2362
Daniel <Daniel@LAPTOP-SALA-2-7>
2023-09-20 09:16:07 +0300

    Add project files.

---
+++ XsiO/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace XsiO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b11 = new System.Windows.Forms.Button();
+            this.b32 = new System.Windows.Forms.Button();
+            this.b31 = new System.Windows.Forms.Button();
+            this.b23 = new System.Windows.Forms.Button();
+            this.b22 = new System.Windows.Forms.Button();
+            this.b21 = new System.Windows.Forms.Button();
+            this.b13 = new System.Windows.Forms.Button();
+            this.b12 = new System.Windows.Forms.Button();
+            this.b33 = new System.Windows.Forms.Button();
+            this.reset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // b11
+            // 
+            this.b11.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b11.Location = new System.Drawing.Point(113, 50);
+            this.b11.Name = "b11";
+            this.b11.Size = new System.Drawing.Size(75, 75);
+            this.b11.TabIndex = 0;
+            this.b11.UseVisualStyleBackColor = true;
+            this.b11.Click += new System.EventHandler(this.b11_Click);
+            // 
+            // b32
+            // 
+            this.b32.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b32.Location = new System.Drawing.Point(238, 274);
+            this.b32.Name = "b32";
+            this.b32.Size = new System.Drawing.Size(75, 75);
+            this.b32.TabIndex = 1;
+            this.b32.UseVisualStyleBackColor = true;
+            // 
+            // b31
+            // 
+            this.b31.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b31.Location = new System.Drawing.Point(113, 274);
+            this.b31.Name = "b31";
+            this.b31.Size = new System.Drawing.Size(75, 75);
+            this.b31.TabIndex = 2;
+            this.b31.UseVisualStyleBackColor = true;
+            // 
+            // b23
+            // 
+            this.b23.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b23.Location = new System.Drawing.Point(363, 165);
+            this.b23.Name = "b23";
+            this.b23.Size = new System.Drawing.Size(75, 75);
+            this.b23.TabIndex = 3;
+            this.b23.UseVisualStyleBackColor = true;
+            // 
+            // b22
+            // 
+            this.b22.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b22.Location = new System.Drawing.Point(238, 165);
+            this.b22.Name = "b22";
+            this.b22.Size = new System.Drawing.Size(75, 75);
+            this.b22.TabIndex = 4;
+            this.b22.UseVisualStyleBackColor = true;
+            this.b22.Click += new System.EventHandler(this.b22_Click);
+            // 
+            // b21
+            // 
+            this.b21.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b21.Location = new System.Drawing.Point(113, 165);
+            this.b21.Name = "b21";
+            this.b21.Size = new System.Drawing.Size(75, 75);
+            this.b21.TabIndex = 5;
+            this.b21.UseVisualStyleBackColor = true;
+            this.b21.Click += new System.EventHandler(this.b21_Click);
+            // 
+            // b13
+            // 
+            this.b13.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b13.Location = new System.Drawing.Point(363, 50);
+            this.b13.Name = "b13";
+            this.b13.Size = new System.Drawing.Size(75, 75);
+            this.b13.TabIndex = 6;
+            this.b13.UseVisualStyleBackColor = true;
+            this.b13.Click += new System.EventHandler(this.b13_Click);
+            // 
+            // b12
+            // 
+            this.b12.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b12.Location = new System.Drawing.Point(238, 50);
+            this.b12.Name = "b12";
+            this.b12.Size = new System.Drawing.Size(75, 75);
+            this.b12.TabIndex = 7;
+            this.b12.UseVisualStyleBackColor = true;
+            this.b12.Click += new System.EventHandler(this.b12_Click);
+            // 
+            // b33
+            // 
+            this.b33.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b33.Location = new System.Drawing.Point(363, 274);
+            this.b33.Name = "b33";
+            this.b33.Size = new System.Drawing.Size(75, 75);
+            this.b33.TabIndex = 8;
+            this.b33.UseVisualStyleBackColor = true;
+            // 
+            // reset
+            // 
+            this.reset.Location = new System.Drawing.Point(567, 50);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(75, 23);
+            this.reset.TabIndex = 9;
+            this.reset.Text = "Reset";
+            this.reset.UseVisualStyleBackColor = true;
+            this.reset.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.b33);
+            this.Controls.Add(this.b12);
+            this.Controls.Add(this.b13);
+            this.Controls.Add(this.b21);
+            this.Controls.Add(this.b22);
+            this.Controls.Add(this.b23);
+            this.Controls.Add(this.b31);
+            this.Controls.Add(this.b32);
+            this.Controls.Add(this.b11);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b11;
+        private System.Windows.Forms.Button b32;
+        private System.Windows.Forms.Button b31;
+        private System.Windows.Forms.Button b23;
+        private System.Windows.Forms.Button b22;
+        private System.Windows.Forms.Button b21;
+        private System.Windows.Forms.Button b13;
+        private System.Windows.Forms.Button b12;
+        private System.Windows.Forms.Button b33;
+        private System.Windows.Forms.Button reset;
+    }
+}
+
---
+++ XsiO/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XsiO
+{
+    public partial class Form1 : Form
+    {
+        int cine;
+        int[] c1;
+        int[] c2;
+        int[] c3;
+        int[] l1;
+        int[] l2;
+        int[] l3;
+        int[] dp;
+        int[] ds;
+     
+
+        private void b13_Click(object sender, EventArgs e)
+        {
+            if (b13.Text == "")
+            {
+                if (cine == 1) b13.Text = "X";
+                else b13.Text = "O";
+
+                c3[cine]++; l1[cine]++; ds[cine]++;
+                if (c1[cine] == 3 || l1[cine] == 3 || ds[cine] == 3)
+                    if (cine == 1) MessageBox.Show("X a castigat");
+                    else MessageBox.Show("0 a castigat");
+                cine = 1 - cine;
+            }
+        }
+
+        private void b12_Click(object sender, EventArgs e)
+        {
+            if (b12.Text == "")
+            {
+                if (cine == 1) b12.Text = "X";
+                else b12.Text = "O";
+
+                c2[cine]++; l1[cine]++;
+                if (c2[cine] == 3 || l1[cine] == 3)
+                    if (cine == 1) MessageBox.Show("X a castigat");
+                    else MessageBox.Show("0 a castigat");
+                cine = 1 - cine;
+            }
+        }
+
+        private void b11_Click(object sender, EventArgs e)
+        {
+            if(b11.Text == "")
+            {
+                if (cine == 1) b11.Text = "X";
+                else b11.Text = "O";
+                
+                c1[cine]++; l1[cine]++; dp[cine]++;
+                if (c1[cine] == 3 || l1[cine] == 3 || dp[cine] == 3)
+                    if (cine == 1) MessageBox.Show("X a castigat");
+                    else MessageBox.Show("0 a castigat");
+                cine = 1 - cine;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            cine = 1; /// 1=x, 0 = 0
+            c1 = new int[2]; c2 = new int[2]; c3 = new int[2];
+            l1 = new int[2]; l2 = new int[2]; l3 = new int[2];
+            ds = new int[2]; dp = new int[2]; 
+            c1[0] = c2[0] = c3[0] = l1[0] = l2[0] = l3[0] = ds[0] = dp[0] = 0;
+            b11.Text = ""; b12.Text = ""; b13.Text = "";
+            b21.Text = ""; b22.Text = ""; b23.Text = "";
+            b31.Text = ""; b32.Text = ""; b33.Text = ""; 
+        }
+
+        private void reset_Click(object sender, EventArgs e)
+        {
+            cine = 1;
+            c1 = new int[2]; c2 = new int[2]; c3 = new int[2];
+            l1 = new int[2]; l2 = new int[2]; l3 = new int[2];
+            ds = new int[2]; dp = new int[2];
+            b11.Text = ""; b12.Text = ""; b13.Text = "";
+            b21.Text = ""; b22.Text = ""; b23.Text = "";
+            b31.Text = ""; b32.Text = ""; b33.Text = "";
+        }
+
+        private void b21_Click(object sender, EventArgs e)
+        {
+            if (b21.Text == "")
+            {
+                if (cine == 1) b21.Text = "X";
+                else b21.Text = "O";
+
+                c1[cine]++; l2[cine]++; 
+                if (c2[cine] == 3 || l1[cine] == 3)
+                    if (cine == 1) MessageBox.Show("X a castigat");
+                    else MessageBox.Show("0 a castigat");
+                cine = 1 - cine;
+            }
+        }
+
+        private void b22_Click(object sender, EventArgs e)
+        {
+            if (b11.Text == "")
+            {
+                if (cine == 1) b11.Text = "X";
+                else b11.Text = "O";
+
+                c1[cine]++; l1[cine]++; dp[cine]++;
+                if (c1[cine] == 3 || l1[cine] == 3 || dp[cine] == 3)
+                    if (cine == 1) MessageBox.Show("X a castigat");
+                    else MessageBox.Show("0 a castigat");
+                cine = 1 - cine;
+            }
+        }
+    }
+}
